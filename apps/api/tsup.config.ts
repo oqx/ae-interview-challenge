c689b22d9006f0171949660e34c10b0655247388
oqx <viamemo@gmail.com>
2024-07-09 17:25:10 -0500

    refactor: CRA -> vite in ui; updates dockerfiles

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error This is used at buildtime, and uses `pnpx` to install in order to add the correct system binaries.
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
